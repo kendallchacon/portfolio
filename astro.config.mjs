dd28ed469eae439e6b712392e50ba4420b44d110
ackerman26lk <kendallchacon4@gmail.com>
2026-04-30 13:32:54 -0600

    feat: add new layout and sections for portfolio
    - Implemented BaseLayout for consistent page structure.
    - Created Navbar component for navigation.
    - Added Hero, About, and Skills sections to showcase portfolio.
    - Included profile image for visual appeal.
    - Updated global styles for smooth scrolling and animations.
    - Added new dependencies for icons and layout enhancements.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
+import icon from "astro-icon";
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    icon(), // 👈 aquí lo agregas
+  ],
 
   vite: {
     plugins: [tailwindcss()],
